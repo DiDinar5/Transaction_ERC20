aa7199181ca9406e9a7373a1526aca4e2cc4a2cf
DiDinar5 <dinar-wp9@mail.ru>
2024-12-06 23:46:39 +0300

    Just simple transaction

---
+++ transaction.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TransferERC20 {
+    event MoneySent(address indexed sender, address indexed recipient, uint256 amount);
+
+    function sendMoney(address payable recipient) public payable {
+        require(msg.value > 0, "You need to send some Ether"); 
+        recipient.transfer(msg.value); 
+        emit MoneySent(msg.sender, recipient, msg.value);
+    }
+}
